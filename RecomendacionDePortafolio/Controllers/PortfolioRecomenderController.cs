56dae9102c024794154db46e70a6742440ecba0d
FernandoSullca <sullcafernando18@gmail.com>
2023-10-29 23:15:57 -0300

    Agregando Rutas para entrenar y solicitar predicciones

--- RecomendacionDePortafolio/Controllers/PortfolioRecomenderController.cs
+++ RecomendacionDePortafolio/Controllers/PortfolioRecomenderController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 using RecomendacionDePortafolio.Services;
 
 namespace RecomendacionDePortafolio.Controllers
@@ -8,13 +9,44 @@ namespace RecomendacionDePortafolio.Controllers
     public class PortfolioRecomenderController : ControllerBase
     {
         private Services.ProductRecommenderIAService _productRecommenderIAService = new ProductRecommenderIAService();
-               
+
+        private readonly ILogger<PortfolioRecomenderController> _logger;
+        public PortfolioRecomenderController(ILogger<PortfolioRecomenderController> logger)
+        {
+            _logger = logger;
+        }
+
 
         [HttpGet("RecomendarPortafolio")]
+        //[ProducesResponseType(StatusCodes.Status200OK, Type = typeof(JsonResult))]
         public JsonResult Get()
         {
             var resultado = _productRecommenderIAService.RecommendTop5(1);
             return resultado;
+        }
+
+        [HttpGet("RecomendarPortafolioConservador")]
+        //[ProducesResponseType(StatusCodes.Status200OK, Type = typeof(JsonResult))]
+        public JsonResult GetConservador()
+        { 
+            
+            return new JsonResult("Recomendación de portafolio conservador");
+        }
+
+        [HttpGet("RecomendarPortafolioModerado")]
+        //[ProducesResponseType(StatusCodes.Status200OK, Type = typeof(JsonResult))]
+        public JsonResult GetModerado()
+        {
+
+            return new JsonResult("Recomendación de portafolio Moderado");
+        }
+
+        [HttpGet("RecomendarPortafolioAgresivo")]
+        //[ProducesResponseType(StatusCodes.Status200OK, Type = typeof(JsonResult))]
+        public JsonResult GetAgresivo()
+        {
+
+            return new JsonResult("Recomendación de portafolio Agresivo");
         }
 
         [HttpPost("EntrenarIA")]
@@ -23,5 +55,43 @@ namespace RecomendacionDePortafolio.Controllers
             _productRecommenderIAService.trainigModelML();
             
         }
+
+        [HttpPost("EntrenarIAConservador")]
+        public void EntrenarIAConservador()
+        {
+            _productRecommenderIAService.trainigModelML();
+
+        }
+        [HttpPost("EntrenarIAModerado")]
+        public void EntrenarIAModerado()
+        {
+            _productRecommenderIAService.trainigModelML();
+
+        }
+        //TODO Funcionalidad mediante Archivo actualizado desde API/JAVA
+        //[HttpPost("EntrenarIAAgresivo")]
+        //[Consumes("multipart/form-data")]
+        //public IActionResult EntrenarIAAgresivo([FromForm] IFormFile file)
+        //{
+        //    if (file != null && file.Length > 0)
+        //    {
+
+        //        var filePath = Path.Combine("./Data", file.FileName);
+
+        //        using (var stream = new FileStream(filePath, FileMode.Create))
+        //        {
+        //            file.CopyTo(stream);
+        //        }
+
+        //        //_productRecommenderIAService.trainigModelML(filePath);
+
+        //        return Ok("Archivo recibido y procesado exitosamente.");
+        //    }
+        //    else
+        //    {
+        //        return BadRequest("No se proporcionó un archivo válido.");
+        //    }
+
+        //}
     }
 }
